935f51cfe99c639b07bcc5b320c7368a2cd7e767
Momo Kornher <mail@moritzkornher.de>
2023-01-10 11:01:18 +0000

    feat: dotnet example setup

---
+++ examples/dotnet-testing/src/DotnetExample/DotnetExampleStack.cs
@@ -0,0 +1,29 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.Events.Targets;
+using Amazon.CDK.AWS.Events;
+using Constructs;
+
+namespace DotnetExample
+{
+    public class DotnetExampleStack : Stack
+    {
+        public DotnetExampleStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            var lambdaFn = new Function(this, "Singleton", new FunctionProps {
+                Runtime = Runtime.PYTHON_3_9,
+                Code = Code.FromInline("def main(event, context):\n" + "    print(\"I'm running!\")\n"),
+                Handler = "index.main",
+                Timeout = Duration.Seconds(300),
+            });
+
+            // Run 6:00 PM UTC every Monday through Friday
+            // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
+            var rule = new Rule(this, "Rule", new RuleProps {
+                Schedule = Schedule.Expression("cron(0 18 ? * MON-FRI *)"),
+            });
+
+            rule.AddTarget(new LambdaFunction(lambdaFn));
+        }
+    }
+}
